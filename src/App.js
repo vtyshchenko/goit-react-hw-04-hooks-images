0b930d2cd7f042325fd2573264855f031fc96c37
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-12-14 00:27:42 +0200

    app done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,129 +15,95 @@ import Spinner from './components/Loader';
 
 import { ReactComponent as IconButtonClose } from './images/icon-close.svg';
 
-const INITIAL_STATE = {
-  status: 'idle',
-  showModal: false,
-  findText: '',
-  images: [],
-  currImg: 0,
-  page: 0,
-};
+function App() {
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const [findText, setFindText] = useState('');
+  const [images, setImages] = useState([]);
+  const [currImg, setCurrImg] = useState(0);
+  const [page, setPage] = useState(0);
 
-class App extends Component {
-  state = {
-    ...INITIAL_STATE,
+  const toggleModal = () => {
+    setShowModal(state => !state);
   };
 
-  static propTypes = {
-    status: PropTypes.oneOf(['idle', 'pending', 'resolved', 'rejected']),
-    showModal: PropTypes.bool,
-    findText: PropTypes.string,
-    images: PropTypes.array,
-    currImg: PropTypes.number,
-    page: PropTypes.number,
-  };
+  const handleGetImages = ({ searchText }) => {
+    const setPageNumber = searchText ? 0 : page;
+    const findingText = searchText ? searchText.toLowerCase().trim() : findText;
 
-  reset = () => {
-    this.setState({
-      ...INITIAL_STATE,
-    });
-  };
+    setStatus('pending');
+    setFindText(findingText);
+    setPage(setPageNumber);
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  handleGetImages = ({ searchText }) => {
-    const setPage = searchText ? 0 : this.state.page;
-    const findText = searchText ? searchText.toLowerCase().trim() : this.state.findText;
-    const setImages =
-      this.state.findText && this.state.findText !== findText ? [] : this.state.images;
-
-    this.setState({
-      status: 'pending',
-      images: setImages,
-      findText: findText,
-      page: setPage,
-    });
-
-    fetchImages(findText, this.state.page + 1, 12)
+    fetchImages(findingText, page + 1, 12)
       .then(response => {
-        this.setState(state => ({
-          status: 'resolved',
-          images: [...state.images, ...response],
-          page: state.page + 1,
-        }));
+        setStatus('resolved');
+        const setImageses = findingText && findText !== findingText ? 0 : 1;
+        setPage(state => state + 1);
 
-        if (this.state.page > 1) {
+        if (setImageses === 1) {
+          setImages([...images, ...response]);
           window.scrollBy({
             top: document.documentElement.clientHeight - 260,
             behavior: 'smooth',
           });
+        } else {
+          setImages([...response]);
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
         }
       })
       .catch(() => {
-        this.setState({
-          status: 'rejected',
-        });
+        setStatus('rejected');
       });
   };
 
-  handleClick = event => {
-    const { images } = this.state;
+  const handleClick = event => {
     const img = images.filter(item => item.id === Number(event.target.attributes.id.nodeValue));
-
-    this.setState({
-      currImg: images.indexOf(img[0]),
-    });
-    img.length > 0 && this.toggleModal(img[0]);
+    setCurrImg(images.indexOf(img[0]));
+    img.length > 0 && toggleModal();
   };
 
-  handleRight = () => {
-    const { images, currImg } = this.state;
-
-    this.setState({
-      currImg: currImg === images.length - 1 ? 0 : currImg + 1,
-    });
+  const handleRight = () => {
+    setCurrImg(state => (state === images.length - 1 ? 0 : state + 1));
   };
 
-  handleLeft = () => {
-    const { images, currImg } = this.state;
-
-    this.setState({
-      currImg: currImg === 0 ? images.length - 1 : currImg - 1,
-    });
+  const handleLeft = () => {
+    setCurrImg(state => (state === 0 ? images.length - 1 : state - 1));
   };
 
-  render() {
-    const { showModal, currImg, images, findText, status } = this.state;
+  const isShow = status === 'resolved' || status === 'pending';
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleGetImages} />
 
-    const isShow = status === 'resolved' || status === 'pending';
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleGetImages} />
-
-        {status === 'rejected' && <p>Error. No images were found for your query: {findText}...</p>}
-        {isShow && (
-          <>
-            <ImageGallery imageList={images} onClick={this.handleClick} />
-            {status === 'pending' ? <Spinner /> : <Button onClick={this.handleGetImages} />}
-          </>
-        )}
-        {showModal && (
-          <Modal onClose={this.toggleModal} onLeft={this.handleLeft} onRight={this.handleRight}>
-            <ButtonClose onClick={this.toggleModal} aria-label="Close modal window">
-              <IconButtonClose fill="black" />
-            </ButtonClose>
-            <img src={images[currImg].largeImageURL} alt={images[currImg].tags} />
-          </Modal>
-        )}
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
+      {status === 'rejected' && <p>Error. No images were found for your query: {findText}...</p>}
+      {isShow && (
+        <>
+          <ImageGallery imageList={images} onClick={handleClick} />
+          {status === 'pending' ? <Spinner /> : <Button onClick={handleGetImages} />}
+        </>
+      )}
+      {showModal && (
+        <Modal onClose={toggleModal} onLeft={handleLeft} onRight={handleRight}>
+          <ButtonClose onClick={toggleModal} aria-label="Close modal window">
+            <IconButtonClose fill="black" />
+          </ButtonClose>
+          <img src={images[currImg].largeImageURL} alt={images[currImg].tags} />
+        </Modal>
+      )}
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
 }
+
+App.propTypes = {
+  status: PropTypes.oneOf(['idle', 'pending', 'resolved', 'rejected']),
+  showModal: PropTypes.bool,
+  findText: PropTypes.string,
+  images: PropTypes.array,
+  currImg: PropTypes.number,
+  page: PropTypes.number,
+};
 
 export default App;
